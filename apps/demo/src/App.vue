31b72bf3ddafd549f3780741271c5f6348601678
Dawntraoz <albasilventefu@gmail.com>
2025-09-01 19:18:14 +0200

    feat: expose the build folder js file from each library, try one library from the workspace in the demo app

--- apps/demo/src/App.vue
+++ apps/demo/src/App.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { summarizerApi } from "@built-in-ai/summarizer-api";
+</script>
 
 <template>
   <h1>You did it!</h1>
+  <p>{{ summarizerApi.name }} (v{{ summarizerApi.version }})</p>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
